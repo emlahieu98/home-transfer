fd4a070c2f55420136c33134a07c93a5d121addc
emlahieu98 <hieucr98tb@gmail.com>
2022-10-15 00:32:21 +0700

    feat: logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Logo from 'assets/images/icon.png'
 class CustomDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
@@ -10,7 +9,6 @@ class CustomDocument extends Document {
         return (
             <Html lang="en-US">
                 <Head>
-                    <link rel="shortcut icon" href={Logo} />
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css2?family=B612:ital,wght@0,400;0,700;1,400;1,700&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
